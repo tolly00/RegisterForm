05eab51dea92baf66dbd5417fbd8c050f69c900c
tolly00 <albertosylveus@gmail.com>
2020-07-28 16:36:27 -0400

    .

--- register.js
+++ register.js
@@ -1,16 +1,30 @@
 
-let firstname=document.getElementById("firstname").value;
-let lastname=document.getElementById("lastname").value;
 
 function validation(){
- if(firstname == ""){
+
+let firstname=document.getElementById("firstname").value;
+let lastname=document.getElementById("lastname").value;
+let pass=document.getElementById("password").value;
+let cpass=document.getElementById("cpassword").value;
+
+ if(firstname === ""){
      document.getElementById("msgfirst").innerText='fill the firstname field';
      return false;
-     ;
- }
+     
+    }
  if(lastname == ""){
     document.getElementById("msglast").innerText='fill the lastname field';
     return false;
-    ;
-}
+    
+    }
+    if(pass == ""){
+        document.getElementById("msgpass").innerText='fill the password field';
+        return false;
+        
+    }
+    if(cpass == ""){
+        document.getElementById("msgcpass").innerText='fill the confirm password field';
+        return false;
+        
+    }
 }
